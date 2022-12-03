6edba4baeb60f2f24b54cc818e57d9c8c2ebd2f9
Sara Bonnin Richaud <saesbori@gmail.com>
2022-12-03 21:07:33 +0000

    add moves_aligned func and unittests to check for risk moves where a gamer has possibilities to win if not neutralised

--- automated_gamer.py
+++ automated_gamer.py
@@ -42,6 +42,32 @@ def win_combinations(game_board, idx_board):
     return combinations
 
 
+def moves_aligned(player_to_check, num_moves, game_board, idx_board):
+    """Given a player to check, num of moves for that player in a winning row, the current game board and idx board,
+    returns the first combination and index locations that matches the player being checked and num of box already
+    filled by that player where the remaining boxes are empty; otherwise returns False"""
+
+    winner_lines = win_combinations(game_board, idx_board)
+
+    for key, values in winner_lines.items():
+        count = 0
+        checks = 1
+        count_empty = 0
+        while count < num_moves and checks < len(winner_lines):
+            for move in values[0]:
+                if str(move).isdigit():
+                    count_empty += 1
+                else:
+                    if move == player_to_check:
+                        count += 1
+
+                if count_empty != 3 and (3 - count - count_empty == 0) and count == num_moves:
+                    return values[0], values[1]
+
+            count = 0
+            count_empty = 0
+            checks += 1
+    return False
 
 # If risk combination is identified, move into the risk box to avoid "S" win
 # If no risk combination is identified, check if "P" has any potential winning combinations
--- test_automated_game.py
+++ test_automated_game.py
@@ -1,5 +1,5 @@
 import unittest
-from automated_gamer import win_combinations, idx_board
+from automated_gamer import win_combinations, idx_board, moves_aligned
 
 board = [[1, 2, 3],
          [4, 5, 6],
@@ -16,6 +16,71 @@ class TestWinCombinations(unittest.TestCase):
                   7: [[1, 5, 9], [[0, 0], [1, 1], [2, 2]]],
                   8: [[3, 5, 7], [[0, 2], [1, 1], [2, 0]]]}
         self.assertEqual(result, win_combinations(board, idx_board))
+
+
+class TestMovesAligned(unittest.TestCase):
+    def test_empty_board_none(self):
+        self.assertEqual(False, moves_aligned("S", 2, board, idx_board))
+
+    def test_1S_2empty_row(self):
+        board_test = [[1, 2, 3],
+                      ["S", 5, 6],
+                      [7, 8, "P"]]
+        self.assertEqual((["S", 5, 6], [[1, 0], [1, 1], [1, 2]]), moves_aligned("S", 1, board_test, idx_board))
+
+    def test_2S_1empty_row(self):
+        board_test = [[1, 2, 3],
+                      ["S", "S", 6],
+                      [7, 8, "P"]]
+        self.assertEqual((["S", "S", 6], [[1, 0], [1, 1], [1, 2]]), moves_aligned("S", 2, board_test, idx_board))
+
+    def test_2S_0empty_row(self):
+        board_test = [[1, 2, 3],
+                      ["S", "S", "P"],
+                      [7, 8, "P"]]
+        self.assertEqual(False, moves_aligned("S", 2, board_test, idx_board))
+
+    def test_1S_2empty_col(self):
+        # should ignore row 2 where ["S", 5, "P"] because S cannot make a line there anymore
+        board_test = [[1, 2, 3],
+                      ["S", 5, "P"],
+                      [7, 8, "P"]]
+        self.assertEqual(([1, "S", 7], [[0, 0], [1, 0], [2, 0]]), moves_aligned("S", 1, board_test, idx_board))
+
+    def test_1S_2empty_diagonal(self):
+        # should ignore row 2 where ["S", 5, "P"] because S cannot make a line there anymore
+        # should ignore col 2 where ["P", "S", 7] because S cannot make a line there anymore
+        board_test = [["P", 2, "S"],
+                      ["S", 5, "P"],
+                      [7, 8, "P"]]
+        self.assertEqual((["S", 5, 7], [[0, 2], [1, 1], [2, 0]]), moves_aligned("S", 1, board_test, idx_board))
+
+    def test_2S_1empty_diagonal(self):
+        # should ignore row 2 where ["S", 5, "P"] because S cannot make a line there anymore
+        # should ignore col 2 where ["P", "S", 7] because S cannot make a line there anymore
+        board_test = [["P", 2, "S"],
+                      ["S", "S", "P"],
+                      [7, 8, "P"]]
+        self.assertEqual((["S", "S", 7], [[0, 2], [1, 1], [2, 0]]), moves_aligned("S", 2, board_test, idx_board))
+
+    def test_2S_1empty_one_movement_left(self):
+        # should ignore row 2 where ["S", 5, "P"] because S cannot make a line there anymore
+        # should ignore col 2 where ["P", "S", 7] because S cannot make a line there anymore
+        board_test = [["P", 2, "S"],
+                      ["S", "S", "P"],
+                      ["P", "S", "P"]]
+        self.assertEqual(([2, "S", "S"], [[0, 1], [1, 1], [2, 1]]), moves_aligned("S", 2, board_test, idx_board))
+
+    def test_no_movements_left(self):
+        # should ignore row 2 where ["S", 5, "P"] because S cannot make a line there anymore
+        # should ignore col 2 where ["P", "S", 7] because S cannot make a line there anymore
+        board_test = [["P", "P", "S"],
+                      ["S", "S", "P"],
+                      ["P", "S", "P"]]
+        self.assertEqual(False, moves_aligned("S", 2, board_test, idx_board))
+
+
+
 
 
 if __name__ == '__main__':
